4f897e17d95fc962d0cfedf9897eb35b7af95fd6
Michelle <falbqui495@g.educaand.es>
2024-01-19 14:34:38 +0100

    aaaaa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { LenguajesComponent } from './componentes/lenguajes/lenguajes.component';
 
-export const routes: Routes = [];
-// aksjhasdkj
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'lenguajes',
+    component: LenguajesComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+];
+// hola
