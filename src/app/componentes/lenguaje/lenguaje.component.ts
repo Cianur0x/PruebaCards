744ef92ad5f2d32af750ccf1ec6aa6ba7c1cbd8c
Michelle <falbqui495@g.educaand.es>
2024-01-29 11:11:44 +0100

    aaa

---
+++ src/app/componentes/lenguaje/lenguaje.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lenguaje',
+  standalone: true,
+  imports: [],
+  templateUrl: './lenguaje.component.html',
+  styleUrl: './lenguaje.component.css'
+})
+export class LenguajeComponent {
+
+}
